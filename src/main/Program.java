18a8fb8e52c3cfea42c6c8ed5a5c2960091f4a8f
saumya14 <saumya14@gmail.com>
2012-09-30 00:07:09 +0530

    Refactoring

--- src/main/Program.java
+++ src/main/Program.java
@@ -1,6 +1,7 @@
 package main;
 
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStreamReader;
 
 public class Program {
@@ -9,43 +10,23 @@ public class Program {
 
     public static void main(String[] args) {
         while (true) {
-            System.out.println("**********************************************************");
-            System.out.println("* Welcome to The Bangalore Public Library System - Biblioteca *");
-            System.out.println("**********************************************************");
-            System.out.println("*                Menu                                    *");
-            System.out.println("*         =====================                          *");
-            System.out.println("*         1. List Book Catalog                           *");
-            System.out.println("*         2. Check out Book                              *");
-            System.out.println("*         3. Check Library Number                        *");
-            System.out.println("*         4. Movie Listing                               *");
-            System.out.println("*         5. Login                                       *");
-            System.out.println("*         9. Exit                                        *");
-            System.out.println("**********************************************************");
-            System.out.println("Your Selection: ");
+            displayMenu();
 
-            InputStreamReader inputStream = new InputStreamReader(System.in);
-            BufferedReader reader = new BufferedReader(inputStream);
             int i1 = 0;
             try {
-                String value = reader.readLine();
+                String value = getUserInput("Your Selection: ");
                 i1 = Integer.parseInt(value);
             } catch (Exception e) {
-                // Do you know what numbers are!!!
                 System.out.println("Enter a valid integer!!");
             }
 
             if (i1 == 1) {
-                System.out.println(" 1. Sweet Valley High vol. 4 by John Travolta ");
-                System.out.println(" 2. eXtreme Programming Explained by Kent Beck ");
-                System.out.println(" 3. How to Win Friends and Influence People by Dale Carnagie ");
-                System.out.println(" 4. How to Cheat at TWU Assignements by Anonymous ");
+                displayBooks();
             } else if (i1 == 2) {
-                System.out.println(" Please enter the number of the book you wish to checkout: ");
                 int i2 = 0;
                 try {
-                    i2 = Integer.parseInt(reader.readLine());
+                    i2 = Integer.parseInt(getUserInput(" Please enter the number of the book you wish to checkout: "));
                 } catch (Exception e) {
-                    // Do you know what numbers are!!!
                     System.out.println("Enter a valid integer!!");
 
                 }
@@ -76,25 +57,14 @@ public class Program {
                     System.out.println("Please talk to Librarian. Thank you.");
                 }
             } else if (i1 == 4) {
-                System.out.println(displayMovieDetails("Rocky", "John G. Avildsen", "10"));
-                System.out.println(displayMovieDetails("Rocky II", "John G. Avildsen", "9"));
-                System.out.println(displayMovieDetails("Rocky III", "John G. Avildsen", "8"));
-                System.out.println(displayMovieDetails("Rocky IV", "John G. Avildsen", "7"));
-                System.out.println(displayMovieDetails("Rocky V", "John G. Avildsen", "8"));
-                System.out.println(displayMovieDetails("Drainage", "Francisco Trindade", "N/A"));
-                System.out.println(displayMovieDetails("The Shawshank Redemption", "Frank Darabont", "10"));
-                System.out.println(displayMovieDetails("The Godfather", "Francis Ford Coppola", "7"));
-                System.out.println(displayMovieDetails("Inception", "Frank Darabont", "10"));
-                System.out.println(displayMovieDetails("Pulp Fiction", "Quentin Tarantino", "6"));
+                displayMovies();
             } else if (i1 == 5) {
                 clearLogin();
-                System.out.println("Enter your library number");
                 try {
-                    String libraryNumber = reader.readLine();
+                    String libraryNumber = getUserInput("Enter your library number");
                     if (validLibraryNumber(libraryNumber)) {
                         try {
-                            System.out.println("Enter your Password: ");
-                            String password = reader.readLine();
+                            String password = getUserInput("Enter your Password:");
                             if (validPassword(password)) {
                                 loggedIn = true;
                                 savedLibraryNumber = libraryNumber;
@@ -115,6 +85,41 @@ public class Program {
                 System.out.println("Enter a valid integer!!");
             }
         }
+    }
+
+    private static void displayBooks() {
+        System.out.println(" 1. Sweet Valley High vol. 4 by John Travolta ");
+        System.out.println(" 2. eXtreme Programming Explained by Kent Beck ");
+        System.out.println(" 3. How to Win Friends and Influence People by Dale Carnagie ");
+        System.out.println(" 4. How to Cheat at TWU Assignements by Anonymous ");
+    }
+
+    private static void displayMenu() {
+        System.out.println("**********************************************************");
+        System.out.println("* Welcome to The Bangalore Public Library System - Biblioteca *");
+        System.out.println("**********************************************************");
+        System.out.println("*                Menu                                    *");
+        System.out.println("*         =====================                          *");
+        System.out.println("*         1. List Book Catalog                           *");
+        System.out.println("*         2. Check out Book                              *");
+        System.out.println("*         3. Check Library Number                        *");
+        System.out.println("*         4. Movie Listing                               *");
+        System.out.println("*         5. Login                                       *");
+        System.out.println("*         9. Exit                                        *");
+        System.out.println("**********************************************************");
+    }
+
+    private static void displayMovies() {
+        System.out.println(displayMovieDetails("Rocky", "John G. Avildsen", "10"));
+        System.out.println(displayMovieDetails("Rocky II", "John G. Avildsen", "9"));
+        System.out.println(displayMovieDetails("Rocky III", "John G. Avildsen", "8"));
+        System.out.println(displayMovieDetails("Rocky IV", "John G. Avildsen", "7"));
+        System.out.println(displayMovieDetails("Rocky V", "John G. Avildsen", "8"));
+        System.out.println(displayMovieDetails("Drainage", "Francisco Trindade", "N/A"));
+        System.out.println(displayMovieDetails("The Shawshank Redemption", "Frank Darabont", "10"));
+        System.out.println(displayMovieDetails("The Godfather", "Francis Ford Coppola", "7"));
+        System.out.println(displayMovieDetails("Inception", "Frank Darabont", "10"));
+        System.out.println(displayMovieDetails("Pulp Fiction", "Quentin Tarantino", "6"));
     }
 
     private static void sucessfulCheckOut() {
@@ -143,5 +148,20 @@ public class Program {
     private static String displayMovieDetails(String movieTitle, String movieDirector, String movieRanking) {
         return movieTitle + " - Director: " + movieDirector + " Rating: " + movieRanking;
     }
+
+    static String getUserInput(String prompt) throws IOException {
+        String inputLine = null;
+        System.out.print(prompt + " ");
+        try {
+            BufferedReader is = new BufferedReader(new InputStreamReader(System.in));
+            inputLine = is.readLine();
+            if (inputLine.length() == 0)
+                return null;
+        } catch (IOException e) {
+            System.out.println("IOException: " + e);
+        }
+        return inputLine;
+    }
+
 }
 
